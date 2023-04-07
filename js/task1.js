ae70134f50a09b1b3701c876dfd155b5d3f962ff
Bogdan Savinov <130148591+YAMAZIK@users.noreply.github.com>
2023-04-07 14:00:17 +0300

    Add files via upload

---
+++ js/task1.js
@@ -0,0 +1,9 @@
+let name = "Генератор захисного поля";
+let price = 1000;
+
+
+console.log("Обрано" + " " + name + " " + price)
+
+ price = 2000;
+
+console.log("Обрано" + " " + name + " " + price)
---
+++ js/task2.js
@@ -0,0 +1,9 @@
+const ordered = 130; //одиниць товару в замовленні
+const total = 100; // кількість товарів на складі
+
+if(total < ordered){
+    console.log("На складі недостатньо товарів")
+
+}else{
+    console.log("Все добре")
+}
